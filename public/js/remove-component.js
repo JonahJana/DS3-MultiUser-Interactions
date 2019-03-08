a06e47e43a3d4a6c8a4de9c5b3cb1fcf05b03709
JonahJana <46599996+JonahJana@users.noreply.github.com>
2019-03-08 18:44:17 -0500

    sounds are now here

--- public/js/remove-component.js
+++ public/js/remove-component.js
@@ -9,9 +9,6 @@ AFRAME.registerComponent('remove-component', {
             console.log("click");
             //object clicked - lets create a cow!
             Context_AF.deleteMyself();
-
-            Context_AF.soundElem.components['sound'].stopSound(); //stop first so we aren't trying to play more than once at same time
-            Context_AF.soundElem.components['sound'].playSound();
         });
     },
     deleteMyself : function() {
