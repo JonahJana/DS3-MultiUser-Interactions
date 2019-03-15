68975ca01a0c707d4f1ac2beca1eb6bf76c3bb43
JonahJana <46599996+JonahJana@users.noreply.github.com>
2019-03-15 18:08:02 -0400

    Revert "Merge branch 'master' of https://github.com/JonahJana/DS3-MultiUser-Interactions"
    This reverts commit 1115a8f68804c82446566229a19fcd22b3eda82f, reversing
    changes made to 2daabd1cd8abeccbdda8cdf0590355c49bfce7d6.

--- public/js/remove-component.js
+++ public/js/remove-component.js
@@ -1,4 +1,9 @@
+<<<<<<< HEAD
 var counter = 0;
+=======
+var counter = 0; 
+var timeoutID;
+>>>>>>> parent of 1115a8f... Merge branch 'master' of https://github.com/JonahJana/DS3-MultiUser-Interactions
 
 AFRAME.registerComponent('remove-component', {
     schema: {},
@@ -13,6 +18,10 @@ AFRAME.registerComponent('remove-component', {
             Context_AF.deleteMyself();
             counter++;
             console.log(counter);
+<<<<<<< HEAD
+=======
+
+>>>>>>> parent of 1115a8f... Merge branch 'master' of https://github.com/JonahJana/DS3-MultiUser-Interactions
         });
     },
     deleteMyself : function() {
@@ -20,4 +29,16 @@ AFRAME.registerComponent('remove-component', {
         Context_AF.el.parentNode.removeChild(Context_AF.el); 
         //note memmory may stay aroudn until "garbage collected" so consider pooling instead of deleting. See note in create-cow element
     }
+<<<<<<< HEAD
 });
+=======
+});
+
+function delayedAlert() {
+  timeoutID = window.setTimeout(window.alert, 2000, 'You iz out of time');
+}
+
+function clearAlert() {
+  window.clearTimeout(timeoutID);
+}
+>>>>>>> parent of 1115a8f... Merge branch 'master' of https://github.com/JonahJana/DS3-MultiUser-Interactions
